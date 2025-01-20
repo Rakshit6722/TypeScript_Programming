779f49687fd13a570b1b8a4f3015086d442fb5f2
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-20 10:14:49 +0530

    delete unnecessary files

--- Generics/defaultGenericTypes.ts
+++
@@ -1,7 +0,0 @@
-interface Response1<T = string>{//we here set a default type for generic which is string
-    data: T;
-    status1: number;
-}
-
-const resposne1: Response1 = {data: "Success", status1:200}
-const response2 :Response1<number> = {data:42, status1:200}
--- Generics/generics.ts
+++
@@ -1,9 +0,0 @@
-//generics -> enable the creation of reusable and type-safe components,
-// allowing types to be passed as arguments
-
-function identity<T>(value: T){
-    return value
-}
-
-console.log(identity<string>("Hello"))
-console.log(identity<number>(42))
