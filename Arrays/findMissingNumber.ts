af3dbd69607f06cc65f43dbbfe4fbdfd6b734473
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-01-21 00:36:05 +0530

    [Rakhsit] Refactor: find missing number

--- Arrays/findMissingNumber.ts
+++ Arrays/findMissingNumber.ts
@@ -4,6 +4,6 @@ const missingNumber = (arr: number[], n: number): number => {
     return total - sum;
 }
 
-let arr: number[] = [1, 2, 3, 4, 6, 7, 8];
+let arr2: number[] = [1, 2, 3, 4, 6, 7, 8];
 let n: number = 8;
-console.log(missingNumber(arr, n)); 
+console.log(missingNumber(arr2, n)); 
